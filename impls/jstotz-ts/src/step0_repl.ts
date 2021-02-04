d86629959dc28e14444fcb299c710edca37512d3
Jay Stotz <jason.stotz@gmail.com>
2021-02-04 11:01:53 -0500

    Implement step 0

--- impls/jstotz-ts/src/step0_repl.ts
+++ impls/jstotz-ts/src/step0_repl.ts
@@ -1,1 +1,30 @@
-console.log("Implement me");
+import readline from "readline";
+
+function read(input: string): string {
+  return input;
+}
+
+function evalInput(input: string): string {
+  return input;
+}
+
+function print(input: string): string {
+  return input;
+}
+
+function rep(input: string): string {
+  return print(evalInput(read(input)));
+}
+
+function startRepl() {
+  let rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt("user> ");
+  rl.on("line", (input) => {
+    let result = rep(input);
+    console.log(result);
+    rl.prompt();
+  });
+  rl.prompt();
+}
+
+startRepl();
