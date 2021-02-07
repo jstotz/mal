37861bf1208831425067a3e5e5b81e32ae4d55c9
Jay Stotz <jason.stotz@gmail.com>
2021-02-07 15:36:39 -0500

    Start step 7

---
+++ impls/jstotz-ts/src/step7_quote.ts
@@ -0,0 +1,304 @@
+import { combine, err, ok, Result } from "neverthrow";
+import readline from "readline";
+import coreEnv from "./core";
+import { MalEnv, malEnvGet, malEnvSet, malNewEnv } from "./env";
+import { MalError, malUnwrapAll } from "./errors";
+import { printForm } from "./printer";
+import { readStr } from "./reader";
+import {
+  MalFunctionDef,
+  malIsSeq,
+  malList,
+  MalList,
+  malNil,
+  malString,
+  MalType,
+} from "./types";
+
+function read(input: string) {
+  return readStr(input);
+}
+
+interface MalState {
+  ast: MalType;
+  env: MalEnv;
+  error?: MalError;
+}
+
+function malEvalAst(ast: MalType, env: MalEnv): Result<MalType, MalError> {
+  switch (ast.type) {
+    case "list":
+    case "vector":
+      return combine(ast.value.map((a) => malEval(a, env))).map((values) => ({
+        type: ast.type,
+        value: values,
+      }));
+    case "hash_map": {
+      return combine(
+        Array.from(ast.value).map(([key, value]) =>
+          malEval(value, env).map((evaled) => ({ key, value: evaled }))
+        )
+      ).map((evaledPairs) => ({
+        type: "hash_map",
+        value: new Map<string, MalType>(
+          evaledPairs.map((pair) => [pair.key, pair.value])
+        ),
+      }));
+    }
+    case "symbol": {
+      let value = malEnvGet(env, ast.value);
+      if (value === undefined) {
+        return err({
+          type: "symbol_not_found",
+          message: `Symbol ${ast.value} not found`,
+        });
+      }
+      return ok(value);
+    }
+    default:
+      return ok(ast);
+  }
+}
+
+function malEvalDef(list: MalList, env: MalEnv): Result<MalType, MalError> {
+  const [, keySymbol, valueAst] = list.value;
+  if (keySymbol?.type !== "symbol") {
+    return err({ type: "type_error", message: "Expected symbol key in def!" });
+  }
+  if (valueAst === undefined) {
+    return err({ type: "type_error", message: "Expected value in def!" });
+  }
+  return malEval(valueAst, env).andThen((evaled) =>
+    ok(malEnvSet(env, keySymbol.value, evaled))
+  );
+}
+
+function malEvalLet(
+  list: MalList,
+  state: MalState
+): Result<MalState, MalError> {
+  const letEnv = malNewEnv(state.env);
+  const [, bindings, body] = list.value;
+  if (bindings?.type !== "list" && bindings?.type !== "vector") {
+    return err({
+      type: "type_error",
+      message: "Expected bindings to be a list or vector in let*",
+    });
+  }
+  if (bindings.value.length % 2 !== 0) {
+    return err({
+      type: "type_error",
+      message: "Binding list in let* has mismatched keys and values",
+    });
+  }
+  if (body === undefined) {
+    return err({
+      type: "type_error",
+      message: "Expected body in let*",
+    });
+  }
+  for (let i = 0; i < bindings.value.length; i += 2) {
+    const bindingKey = bindings.value[i];
+    const bindingValue = bindings.value[i + 1];
+    if (bindingKey.type !== "symbol") {
+      return err({
+        type: "type_error",
+        message: `Expected let* binding key to be a symbol. Got: ${printForm(
+          bindingKey
+        )}`,
+      });
+    }
+    const evaledBindingValue = malEval(bindingValue, letEnv);
+    if (evaledBindingValue.isErr()) return err(evaledBindingValue.error);
+    malEnvSet(letEnv, bindingKey.value, evaledBindingValue.value);
+  }
+  return ok({
+    ast: body,
+    env: letEnv,
+  });
+}
+
+function malEvalDo(list: MalList, state: MalState): Result<MalState, MalError> {
+  for (let i = 1; i < list.value.length; i++) {
+    const form = list.value[i];
+    const evalResult = malEval(form, state.env);
+    if (evalResult.isErr()) return err(evalResult.error);
+    if (i === list.value.length - 1) {
+      return ok({ ...state, ast: evalResult.value });
+    }
+  }
+  return ok({ ...state, ast: malNil() });
+}
+
+function malCastBoolean(ast: MalType): boolean {
+  if (ast.type === "boolean") return ast.value;
+  if (ast.type === "nil") return false;
+  return true;
+}
+
+function malEvalIf(list: MalList, state: MalState): Result<MalState, MalError> {
+  const [, cond, ifBody, elseBody] = list.value;
+  if (ifBody === undefined) {
+    return err({ type: "type_error", message: "if called without a body" });
+  }
+  const condResult = malEval(cond, state.env);
+  if (condResult.isErr()) return err(condResult.error);
+  if (malCastBoolean(condResult.value)) {
+    return ok({ ...state, ast: ifBody });
+  } else {
+    return ok({ ...state, ast: elseBody ?? malNil() });
+  }
+}
+
+function malEvalFnDef(
+  list: MalList,
+  outerEnv: MalEnv
+): Result<MalFunctionDef, MalError> {
+  const [, bindings, body] = list.value;
+  if (!malIsSeq(bindings)) {
+    return err({
+      type: "type_error",
+      message: "function bindings must be a sequence",
+    });
+  }
+  return malUnwrapAll("symbol", bindings.value).andThen((bindingKeys) =>
+    ok({
+      type: "function_def",
+      value: {
+        body,
+        env: outerEnv,
+        paramNames: bindingKeys,
+        function: {
+          type: "function",
+          value: (...args) => {
+            const env = malNewEnv(outerEnv, bindingKeys, args);
+            return malEval(body, env);
+          },
+        },
+      },
+    })
+  );
+}
+
+function malEval(ast: MalType, env: MalEnv): Result<MalType, MalError> {
+  let state: MalState = { ast: ast, env: env };
+  const updateState = (result: Result<MalState, MalError>) => {
+    if (result.isErr()) {
+      state.error = result.error;
+      return;
+    }
+    state = result.value;
+  };
+  while (true) {
+    const { ast, env, error } = state;
+    if (error !== undefined) {
+      return err(error);
+    }
+    if (ast.type !== "list") {
+      return malEvalAst(ast, env);
+    }
+
+    if (ast.value.length === 0) return ok(ast);
+    if (ast.value[0].type === "symbol") {
+      switch (ast.value[0].value) {
+        case "def!":
+          return malEvalDef(ast, env);
+        case "let*":
+          updateState(malEvalLet(ast, state));
+          continue;
+        case "do":
+          updateState(malEvalDo(ast, state));
+          continue;
+        case "if":
+          updateState(malEvalIf(ast, state));
+          continue;
+        case "fn*":
+          return malEvalFnDef(ast, env);
+      }
+    }
+    // apply
+    let evalResult = malEvalAst(ast, env);
+    if (evalResult.isErr()) return evalResult;
+    const list = evalResult.value;
+    if (list.type !== "list") {
+      throw new Error(
+        `evalAst returned an unexpected type: ${printForm(list)}`
+      );
+    }
+    if (list.value.length !== ast.value.length) {
+      throw new Error("evalAst returned a different length list");
+    }
+    const [malFn, ...args] = list.value;
+    switch (malFn.type) {
+      case "function":
+        return malFn.value(...args);
+      case "function_def":
+        state = {
+          ...state,
+          ast: malFn.value.body,
+          env: malNewEnv(malFn.value.env, malFn.value.paramNames, args),
+        };
+        continue;
+    }
+  }
+}
+
+function print(
+  evalResult: Result<MalType, MalError>
+): Result<string, MalError> {
+  return evalResult.map((form) => {
+    if (process.env.DEBUG) {
+      // console.debug(debugForm(form));
+    }
+    return printForm(form);
+  });
+}
+
+function rep(input: string): Result<string, MalError> {
+  return read(input).andThen((form) => print(malEval(form, coreEnv)));
+}
+
+function startRepl() {
+  const mustEval = (input: string) =>
+    read(input)
+      .andThen((form) => malEval(form, coreEnv))
+      .orElse((e) => {
+        throw e;
+      });
+
+  mustEval("(def! not (fn* (a) (if a false true)))");
+  mustEval(
+    '(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) "\nnil)")))))'
+  );
+
+  malEnvSet(coreEnv, "eval", {
+    type: "function",
+    value: (ast) => malEval(ast, coreEnv),
+  });
+
+  malEnvSet(
+    coreEnv,
+    "*ARGV*",
+    malList(process.argv.slice(3).map((arg) => malString(arg)))
+  );
+
+  if (process.argv.length > 2) {
+    mustEval(`(load-file ${printForm(malString(process.argv[2]))})`);
+    return;
+  }
+
+  let rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt("user> ");
+  rl.on("line", (input) => {
+    rep(input).match(
+      (output) => {
+        console.log(output);
+      },
+      (error) => console.log("\x1b[31mERROR: %s\x1b[0m", error.message)
+    );
+    rl.prompt();
+  });
+  rl.prompt();
+}
+
+startRepl();
